9eeb29bb80871aa6cb1f7cae1fddc7552005f091
kahmannf <felix@kahmann.de>
2018-10-02 18:10:04 +0200

    add the Total property to the page class

--- backend/net core/GlossaryDefinition/Page.cs
+++ backend/net core/GlossaryDefinition/Page.cs
@@ -12,5 +12,6 @@ namespace GlossaryDefinition
         public T[] Items { get; set; }
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
+        public int Total { get; set; }
     }
 }
